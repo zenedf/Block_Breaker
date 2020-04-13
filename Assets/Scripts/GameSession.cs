b1a617fbb88ac262c6e4d40a99c470492a80c673
Ethan Frazier <zenedf@protonmail.com>
2020-04-12 23:06:35 -0400

    I made a scene full dynamic :D

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -17,7 +17,7 @@ public class GameSession : MonoBehaviour
 
     // config params
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
-    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] int pointsPerBlockDestroyed = 10;
     [SerializeField] TextMeshProUGUI txtScore;
     [SerializeField] TextMeshProUGUI txtBlockCount;
 
