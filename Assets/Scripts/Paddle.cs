6802f42450f96a2e5e13dae9257451465b3438ec
Ethan Frazier <13003531+zenedf@users.noreply.github.com>
2020-04-15 21:46:12 -0400

    code_cleanup_Paddle.cs

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,35 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// This handles the paddle.
+/// This handles the player controlled paddle.
 /// </summary>
 public class Paddle : MonoBehaviour
 {
     #region Configuration Parameters
+    
     // Screen width of the current window in units.
     [SerializeField] private readonly float screenWidthInUnits = 16f;
 
-    // How far left paddle can go.
+    // How far left the paddle can go.
     [SerializeField] private readonly float xMin = 1f;
 
     // How far right the paddle can go.
     [SerializeField] private readonly float xMax = 15f;
-    #endregion
+    
+    #endregion Configuration Parameters
 
     /// <summary>
     /// Update is called once per frame
     /// </summary>
     void Update()
     {
-        //Debug.Log("Paddle.Update()");
-
-
+        #region Screen Information Notes
+        
         // 'Screen.width' shows the current width of the screen window in pixels.
-        // We know that there are currently 16 units. We have 16 Unity units from left to right due to how we set up the camera.
+        //  We know that there are currently 16 units. We have 16 Unity units from left to right due to how we set up the camera.
         // (Camera is the size of 6, which is the vertical. That means the total vertical is 12.)
         // (We have a 4:3 aspect ratio. So, given that our camera size is 6, our width is 16. Half of our width is going to be 8.)
+        
+        #endregion Screen Information Notes
 
-        // This variable shows wherever your mouse currently is in relation to the middle of the game window.
+        // This variable shows the mouse location in relation to the middle of the game window.
         float _mousePositionInUnits = (Input.mousePosition.x / Screen.width) * screenWidthInUnits;
 
         // This gets the current X and Y positions of the paddle
@@ -43,7 +46,6 @@ public class Paddle : MonoBehaviour
 
         // This makes the paddle move
         transform.position = _paddlePosition;
-
 
         #region Testing pressing the pause key in Paddle.cs
 
@@ -64,6 +66,6 @@ public class Paddle : MonoBehaviour
         //    }
         //}
 
-        #endregion
+        #endregion Testing pressing the pause key in Paddle.cs
     }
 }
